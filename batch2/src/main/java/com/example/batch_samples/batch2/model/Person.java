53922d3d1c5b9d484ffc55d25ab144dafd2af63e
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-08-13 17:49:35 +0200

    Added batch2 module.

---
+++ batch2/src/main/java/com/example/batch_samples/batch2/model/Person.java
@@ -0,0 +1,16 @@
+package com.example.batch_samples.batch2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private int age;
+    private String firstName;
+    private String email;
+
+
+}
